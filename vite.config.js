e2a71a5f8760c455c18ce93ff95dfae29443bab3
TaeJoo Kim <taejook1107@gmail.com>
2024-07-17 02:05:22 -0400

    changed title

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/se_project_react/',
   // add the server object
   server: {
     port: 3000,

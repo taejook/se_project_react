8e4eab1318eb8e62fa5702d67f91326196fe9fe6
TaeJoo Kim <taejook1107@gmail.com>
2025-07-31 20:21:03 -0400

    added crash test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/se_project_react/',
+  base: '/',
   // add the server object
   server: {
     port: 3000,
